323fc336bff5ab9635c76bf8c0a8856c6f96fa1f
Fatur Gautama <fatur@paycombat.com>
2025-04-30 13:56:47 +0700

    feat: enhance cron invoice

--- src/invoice/invoice-cron.service.ts
+++ src/invoice/invoice-cron.service.ts
@@ -8,7 +8,6 @@ import { InvoiceService } from './invoice.service';
 
 @Injectable()
 export class InvoiceCronService {
-
     private sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
     constructor(
@@ -19,123 +18,104 @@ export class InvoiceCronService {
         private _settingCompanyService: SettingCompanyService,
     ) { }
 
-    // Cron job to run on the 5th of each month at 07:00 WIB
-    // @Cron('0 0 7 5 * *', {
-    //     timeZone: 'Asia/Jakarta',
-    // })
-    @Cron('0 0 0 29 * *', {
-        timeZone: 'Asia/Jakarta',
-    })
-    async handleSendWhatsappJob() {
-        console.log('Running job to create invoices and send WhatsApp messages.');
+    @Cron('0 0 0 * * *', { timeZone: 'Asia/Jakarta' }) // Setiap hari jam 07:00 WIB
+    async handleDailyTagihanCheck() {
+        const today = new Date();
+        const currentDate = today.getDate(); // ambil tanggal (1 - 31)
 
-        // Step 1: Automatically create invoices for all customers
-        const pelangganProduct = await this._prismaService.pelanggan_product.findMany({
+        const companies = await this._prismaService.setting_company.findMany({
             where: {
                 is_active: true,
+                tagihan_jatuh_tempo: currentDate,
             },
-            include: {
-                pelanggan: {
-                    include: {
-                        setting_company: true
-                    }
-                }
-            }
         });
 
-        console.log("pelanggan product =>", JSON.stringify(pelangganProduct));
-
-        for (const product of pelangganProduct) {
-            const invoice_date = new Date();
-            const year = invoice_date.getFullYear();
-            const month = invoice_date.getMonth() + 1;
-            const startDate = new Date(year, month, 1);
-            const endDate = new Date(year, month + 1, 1);
-            const setting_company = await this._prismaService.setting_company.findFirst({
-                where: {
-                    id_setting_company: parseInt(product.pelanggan.setting_company.id_setting_company as any)
-                },
-                select: {
-                    tagihan_jatuh_tempo: true
-                }
-            });
-            const bulanJatuhTempo = invoice_date.getMonth() + 2;
-            const due_date = new Date(year, bulanJatuhTempo - 1, setting_company.tagihan_jatuh_tempo, 0, 0, 0);
-            const invoiceMonthCount = await this._prismaService
-                .invoice
-                .count({
+        for (const company of companies) {
+            // Lewati jika hari ini masih sebelum tagihan_jatuh_tempo
+            if (currentDate <= company.tagihan_jatuh_tempo) {
+                const customers = await this._prismaService.pelanggan.findMany({
                     where: {
-                        invoice_date: {
-                            gt: startDate,
-                            lt: endDate,
-                        }
-                    }
+                        is_active: true,
+                        id_setting_company: company.id_setting_company,
+                    },
+                    include: {
+                        pelanggan_product: true,
+                    },
                 });
-            const prefix = invoiceMonthCount < 10 ? '000' :
-                (invoiceMonthCount < 100 ? '00' :
-                    (invoiceMonthCount < 1000 ? '0' : '')
-                );
-            const invoicePayload = {
-                invoice_date: new Date(), // set your desired invoice date here
-                invoice_number: `INV-${product.id_pelanggan}-${month > 9 ? month : `0${month}`}${year}-${prefix}${invoiceMonthCount + 1}`, // set your desired invoice date here
-                id_pelanggan: product.id_pelanggan,
-                id_pelanggan_product: product.id_pelanggan_product,
-                id_product: product.id_product,
-                price: product.price,
-                diskon_percentage: 0,
-                diskon_rupiah: 0,
-                pajak: 0,
-                admin_fee: 0,
-                unique_code: "-",
-                total: product.price,
-                due_date: due_date,
-                invoice_status: 'PENDING',
-                create_at: new Date(),
-                create_by: 9999,
-            };
 
-            console.log("invoice payload cron =>", invoicePayload);
+                for (const customer of customers) {
+                    for (const product of customer.pelanggan_product) {
+                        const existingInvoice = await this._prismaService.invoice.findFirst({
+                            where: {
+                                id_pelanggan: customer.id_pelanggan,
+                                id_product: product.id_product,
+                                invoice_date: {
+                                    gte: new Date(today.getFullYear(), today.getMonth(), 1),
+                                    lt: new Date(today.getFullYear(), today.getMonth() + 1, 1),
+                                },
+                                is_deleted: false,
+                            },
+                        });
 
-            // Create invoice for each customer
-            const createdInvoice = await this.createInvoice(invoicePayload);
+                        let id_invoice = existingInvoice?.id_invoice;
 
-            console.log("result invoice cron =>", createdInvoice);
+                        if (!existingInvoice) {
+                            const now = new Date();
+                            const year = now.getFullYear();
+                            const month = now.getMonth(); // 0-based
+                            const startDate = new Date(year, month, 1);
+                            const endDate = new Date(year, month + 1, 1);
+                            const invoiceMonthCount = await this._prismaService.invoice.count({
+                                where: {
+                                    invoice_date: {
+                                        gt: startDate,
+                                        lt: endDate,
+                                    },
+                                },
+                            });
 
-            // Step 2: Send WhatsApp messages for each created invoice
-            if (createdInvoice) {
-                await this.sendPendingWhatsappMessages(createdInvoice.id_invoice);
+                            const paddedCount = String(invoiceMonthCount + 1).padStart(4, '0');
+                            const maxDay = new Date(year, month + 1, 0).getDate();
+                            const dueDay = Math.min(company.tagihan_jatuh_tempo, maxDay);
+                            const due_date = new Date(year, month + 1, dueDay); // jatuh tempo bulan depan
+
+                            const createdInvoice = await this._prismaService.invoice.create({
+                                data: {
+                                    invoice_date: now,
+                                    invoice_number: `INV-${product.id_pelanggan}-${String(month + 1).padStart(2, '0')}${year}-${paddedCount}`,
+                                    id_pelanggan: product.id_pelanggan,
+                                    id_pelanggan_product: product.id_pelanggan_product,
+                                    id_product: product.id_product,
+                                    price: product.price,
+                                    diskon_percentage: 0,
+                                    diskon_rupiah: 0,
+                                    pajak: 0,
+                                    admin_fee: 0,
+                                    unique_code: "-",
+                                    total: product.price,
+                                    due_date: due_date,
+                                    invoice_status: 'PENDING',
+                                    create_at: now,
+                                    create_by: 9999,
+                                },
+                            });
+
+                            id_invoice = createdInvoice.id_invoice;
+                        }
+
+                        // Kirim WA
+                        await this._invoiceService.sendMessage(id_invoice);
+                        await this.sleep(20000); // tunggu 20 detik sebelum kirim berikutnya
+                    }
+                };
+            }
+
+            // Validasi: apakah tagihan_jatuh_tempo valid di bulan ini?
+            const maxDayThisMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+            if (company.tagihan_jatuh_tempo > maxDayThisMonth) {
+                console.warn(`Lewati ${company.company_name} karena tagihan_jatuh_tempo (${company.tagihan_jatuh_tempo}) melebihi jumlah hari di bulan ini (${maxDayThisMonth}).`);
+                continue;
             }
         }
     }
-
-    async createInvoice(payload: any): Promise<any> {
-        try {
-            const invoice = await this._prismaService.invoice.create({
-                data: payload,
-            });
-
-            return invoice;
-        } catch (error) {
-            console.error('Error creating invoice:', error);
-            return null;
-        }
-    }
-
-    async sendPendingWhatsappMessages(id_invoice: number) {
-        await this._invoiceService.sendMessage(id_invoice);
-        await this.sleep(20000); // wait 20 seconds before next message
-    }
-
-    // @Cron('0 * * * *') // Every minute
-    // async testCron() {
-    //     console.log('Test cron running');
-
-    //     const pendingInvoices = await this._prismaService.invoice.findMany({
-    //         where: {
-    //             invoice_status: 'PENDING',
-    //         },
-    //     });
-
-    //     console.log('Pending invoices:', pendingInvoices.length);
-    // }
 }
