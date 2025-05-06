3590235a6000fbffe51da18945c0fd3258d05975
Fatur Gautama <fatur@paycombat.com>
2025-05-06 21:02:40 +0700

    feat: test cron

---
+++ src/module/group-pelanggan/group-pelanggan.service.ts
@@ -0,0 +1,174 @@
+import { HttpException, HttpStatus, Injectable, Scope } from '@nestjs/common';
+import { Request } from 'express';
+import { PrismaService } from 'src/prisma.service';
+import { GroupPelangganModel } from './group-pelanggan.model';
+import { SettingCompanyService } from '../setting-company/setting-company.service';
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class GroupPelangganService {
+
+    constructor(
+        private _prismaService: PrismaService,
+        private _settingCompanyService: SettingCompanyService,
+    ) { }
+
+    async getAll(req: Request, query: GroupPelangganModel.IGroupPelangganQueryParams): Promise<GroupPelangganModel.GetAllGroupPelanggan> {
+        try {
+            let queries: any = { ...query }
+
+            const setting_company = await this._settingCompanyService.getById(parseInt(req['user']['id_setting_company']));
+
+            if (setting_company.status) {
+                if (!setting_company.data.is_cabang && !setting_company.data.is_mitra) {
+                    // ** do nothing
+                };
+
+                // ** Queries id_setting_company
+                if (setting_company.data.is_cabang || setting_company.data.is_mitra) {
+                    queries.id_setting_company = setting_company.data.id_setting_company;
+                }
+            }
+
+            let res: any[] = await this._prismaService
+                .group_pelanggan
+                .findMany({
+                    where: Object.keys(queries).reduce((aggregate, property) => {
+                        if (property == 'id_kelas') {
+                            aggregate[property] = parseInt(query[property] as any);
+                        }
+                        return aggregate;
+                    }, {}),
+                    orderBy: {
+                        id_group_pelanggan: 'asc'
+                    }
+                });
+
+            return {
+                status: true,
+                message: '',
+                data: res
+            }
+
+        } catch (error) {
+            const status = error.message.includes('not found')
+                ? HttpStatus.NOT_FOUND
+                : error.message.includes('bad request')
+                    ? HttpStatus.BAD_REQUEST
+                    : HttpStatus.INTERNAL_SERVER_ERROR;
+
+            throw new HttpException(
+                {
+                    status: false,
+                    message: error.message
+                },
+                status
+            );
+        }
+    }
+
+    async getById(id_group_pelanggan: number): Promise<GroupPelangganModel.GetByIdGroupPelanggan> {
+        try {
+            let res: any = await this._prismaService
+                .group_pelanggan
+                .findUnique({
+                    where: { id_group_pelanggan: parseInt(id_group_pelanggan as any) }
+                });
+
+            return {
+                status: true,
+                message: '',
+                data: res
+            }
+
+        } catch (error) {
+            const status = error.message.includes('not found')
+                ? HttpStatus.NOT_FOUND
+                : error.message.includes('bad request')
+                    ? HttpStatus.BAD_REQUEST
+                    : HttpStatus.INTERNAL_SERVER_ERROR;
+
+            throw new HttpException(
+                {
+                    status: false,
+                    message: error.message
+                },
+                status
+            );
+        }
+    }
+
+    async create(req: Request, payload: GroupPelangganModel.CreateGroupPelanggan): Promise<any> {
+        try {
+            let res = await this._prismaService
+                .group_pelanggan
+                .create({
+                    data: {
+                        ...payload,
+                        id_setting_company: req['user']['id_setting_company'],
+                        create_at: new Date(),
+                        create_by: parseInt(req['user']['id_user'] as any)
+                    }
+                })
+
+            return {
+                status: true,
+                message: '',
+                data: res
+            }
+
+        } catch (error) {
+            const status = error.message.includes('not found')
+                ? HttpStatus.NOT_FOUND
+                : error.message.includes('bad request')
+                    ? HttpStatus.BAD_REQUEST
+                    : HttpStatus.INTERNAL_SERVER_ERROR;
+
+            throw new HttpException(
+                {
+                    status: false,
+                    message: error.message
+                },
+                status
+            );
+        }
+    }
+
+    async update(req: Request, payload: GroupPelangganModel.UpdateGroupPelanggan): Promise<any> {
+        try {
+            const { id_group_pelanggan, ...data } = payload
+
+            let res = await this._prismaService
+                .group_pelanggan
+                .update({
+                    where: { id_group_pelanggan: parseInt(id_group_pelanggan as any) },
+                    data: {
+                        ...data,
+                        id_setting_company: req['user']['id_setting_company'],
+                        update_at: new Date(),
+                        update_by: parseInt(req['user']['id_user'] as any)
+                    }
+                })
+
+            return {
+                status: true,
+                message: '',
+                data: res
+            }
+
+        } catch (error) {
+            const status = error.message.includes('not found')
+                ? HttpStatus.NOT_FOUND
+                : error.message.includes('bad request')
+                    ? HttpStatus.BAD_REQUEST
+                    : HttpStatus.INTERNAL_SERVER_ERROR;
+
+            throw new HttpException(
+                {
+                    status: false,
+                    message: error.message
+                },
+                status
+            );
+        }
+    }
+}
---
+++ src/scheduler/send-message-cron.service.ts
@@ -0,0 +1,129 @@
+import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { PrismaService } from 'src/prisma.service';
+import { firstValueFrom } from 'rxjs';
+import { AxiosService } from 'src/helper/utility/axios.service';
+import { UtilityService } from 'src/helper/utility/utility.service';
+
+@Injectable()
+export class SendMessageCronService {
+    private sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    constructor(
+        private _axiosService: AxiosService,
+        private _prismaService: PrismaService,
+        private _utilityService: UtilityService,
+    ) { }
+
+    @Cron('* * * * * *', { timeZone: 'Asia/Jakarta', name: 'send_invoice_notifications' })
+    async sendInvoiceNotifications() {
+        console.log("Starting cron sending message.....")
+
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = today.getMonth(); // 0-based
+
+        // Rentang waktu: 4 tanggal bulan ini (00:00:00) s.d. 5 tanggal bulan ini (00:00:00)
+        const start = new Date(year, month, 1, 0, 0, 0);
+        const end = new Date(year, month, 5, 0, 0, 0);
+
+        const invoices = await this._prismaService.invoice.findMany({
+            where: {
+                invoice_date: {
+                    gte: start,
+                    lt: end,
+                },
+                is_deleted: false,
+            },
+            include: {
+                pelanggan: {
+                    include: {
+                        setting_company: true
+                    }
+                },
+                product: true,
+            }
+        });
+
+        for (const invoice of invoices) {
+            // await this.sendMessage(invoice);
+            console.log("sending invoice to =>", invoice.pelanggan.full_name);
+            await this.sleep(20000); // tunggu 20 detik
+        }
+    }
+
+    async sendMessage(invoice: any): Promise<any> {
+        if (!invoice.pelanggan.setting_company.api_key_wa) {
+            return {
+                status: false,
+                message: "API Key WA belum diatur"
+            }
+        };
+
+        const token = this._utilityService.onEncrypt(JSON.stringify(invoice.id_invoice));
+
+        const messageVariable = {
+            full_name: invoice.pelanggan.full_name,
+            pelanggan_code: invoice.pelanggan.pelanggan_code,
+            product_name: invoice.product.product_name,
+            invoice_date: this._utilityService.onFormatDate(new Date(invoice.invoice_date), 'MMM yyyy'),
+            invoice_number: invoice.invoice_number,
+            total: this._utilityService.onFormatCurrency(invoice.total),
+            checkout_url: `${process.env.CHECKOUT_URL}?token=${token}`,
+        }
+
+        const template = invoice.pelanggan.setting_company.tagihan_pesan_invoice;
+        const newTemplate = template.replace(/\${(.*?)}/g, (_, key) => messageVariable[key.trim()] || "");
+        const messageText = newTemplate
+            .replace(/<\/p>\s*<p>/g, '\n') // Replace consecutive <p> tags with a single line break
+            .replace(/<\/?[^>]+(>|$)/g, "") // Remove any remaining HTML tags
+            .replace(/&nbsp;/g, ' ') // Replace non-breaking spaces with normal spaces
+            .replace(/&gt;/g, '>') // Replace `&gt;` with `>`
+            .replace(/&lt;/g, '<') // Replace `&lt;` with `<`
+            .replace(/&amp;/g, '&') // Replace `&amp;` with `&`
+            .trim(); // Remove any leading or trailing spaces
+
+        const payloadSendMessageMpwa = {
+            method: 'get',
+            url: `${process.env.MPWA_URL}/send-message`,
+            params: {
+                api_key: invoice.pelanggan.setting_company.api_key_wa,
+                sender: invoice.pelanggan.setting_company.company_whatsapp,
+                number: invoice.pelanggan.whatsapp,
+                message: messageText,
+            }
+        };
+
+        const mpwaSendMessageResult = await firstValueFrom(this._axiosService.onAxiosRequest(payloadSendMessageMpwa));
+
+        console.log("result send wa =>", mpwaSendMessageResult);
+
+        if (!mpwaSendMessageResult.status) {
+            await this._prismaService
+                .log_whatsapp_message
+                .create({
+                    data: {
+                        id_invoice: invoice.id_invoice,
+                        id_setting_company: invoice.pelanggan.id_setting_company,
+                        additional_info: invoice,
+                        sent_at: new Date(),
+                        sent_by: invoice.create_by,
+                        status: 'FAILED'
+                    }
+                });
+        } else {
+            await this._prismaService
+                .log_whatsapp_message
+                .create({
+                    data: {
+                        id_invoice: invoice.id_invoice,
+                        id_setting_company: invoice.pelanggan.id_setting_company,
+                        additional_info: invoice,
+                        sent_at: new Date(),
+                        sent_by: invoice.create_by,
+                        status: 'SUCCESS'
+                    }
+                });
+        }
+    }
+}
